81a02fd17446970c598439272418c414119761c8
ikkei12 <ikkei12.h@gmail.com>
2020-12-28 19:08:00 +0900

    add global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [{ src: '~/assets/scss/global.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['@/plugins/composition-api', '@/plugins/axios'],
