694cd1f98222463448e8242414fc9ee7fd987658
ikkei12 <ikkei12.h@gmail.com>
2020-12-28 19:52:34 +0900

    add pages

---
+++ pages/articles.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  // setup(_props, context) {
+  //   context.root.$axios
+  //     .get('/products')
+  //     .then((res) => console.log(res.data))
+  //     .catch((e) => console.log(e))
+  // },
+})
+</script>
+
+<style></style>
