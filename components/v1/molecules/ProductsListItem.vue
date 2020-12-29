fce97e995cfe600a97e917044faed5ac31abde7d
ikkei12 <ikkei12.h@gmail.com>
2020-12-29 15:29:59 +0900

    add list item

---
+++ components/v1/molecules/ProductsListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>{{ product.title }}</p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      required: true,
+    },
+  },
+})
+</script>
--- components/v1/organisms/ProductsList.vue
+++ components/v1/organisms/ProductsList.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <ProductsListItem
-      v-for="(product, i) in products"
-      :key="'product' + i"
-      :product="product"
-    />
+    <ProductsListThumbnail :url="" />
+    <div>
+      <ProductsListItem
+        v-for="(product, i) in products"
+        :key="'product' + i"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api'
 import ProductsListItem from '@/components/v1/molecules/ProductsListItem.vue'
+import ProductsListThumbnail from '@/components/v1/atoms/ProductsListThumbnail.vue'
+
 export default defineComponent({
   components: {
     ProductsListItem,
+    ProductsListThumbnail,
   },
   props: {
     products: {
