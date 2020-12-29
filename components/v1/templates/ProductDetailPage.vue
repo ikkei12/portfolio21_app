b4708693e6d18c2c5ba6792ecd6c46c533f3aad9
ikkei12 <ikkei12.h@gmail.com>
2020-12-29 22:44:43 +0900

    add swiper component

--- components/v1/templates/ProductDetailPage.vue
+++ components/v1/templates/ProductDetailPage.vue
@@ -1,25 +1,54 @@
 <template>
   <div class="product-detail__page">
-    <p>{{ product.title }}</p>
+    <swiper ref="swiperRef" :options="swiperOptions">
+      <swiper-slide
+        v-for="(productContent, i) in product.productContents"
+        :key="'slide' + i"
+      >
+        <div>
+          <img :src="productContent.image.url" style="width: 100%" />
+        </div>
+      </swiper-slide>
+    </swiper>
+    <Pagination current="" :length="product.productContents.length" />
+    <div class="swiper-button-prev" @click="slidePrev" />
+    <div class="swiper-button-next" @click="slideNext" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from '@vue/composition-api'
+import { defineComponent, PropType, ref } from '@vue/composition-api'
+import { Pagination } from '@/components/v1/molecules/Pagination.vue'
 
 export default defineComponent({
+  components: { Pagination },
   props: {
     product: {
       type: Object as PropType<Product>,
       require: true,
     },
   },
-  setup(_props) {},
+  setup(_props, _context) {
+    const swiperRef = ref<HTMLImageElement>({})
+    const swiperOptions = {
+      loop: true,
+    }
+    const slideNext = () => {
+      swiperRef.value.$swiper.slideNext()
+    }
+    const slidePrev = () => {
+      swiperRef.value.$swiper.slidePrev()
+    }
+
+    return { slideNext, slidePrev, swiperRef, swiperOptions }
+  },
 })
 </script>
 <style scoped lang="scss">
 .product-detail__page {
   padding: 0 8vw;
   padding-top: 60px;
+  .swiper-button-next {
+  }
 }
 </style>
---
+++ plugins/vue-awesome-swiper.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import '@/node_modules/swiper/swiper.min.css'
+import '@/node_modules/swiper/swiper-bundle.min.css'
+
+Vue.use(VueAwesomeSwiper)
