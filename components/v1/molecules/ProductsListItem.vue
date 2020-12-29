98bf4f817bdf2c41d9ee7616127fd725ebebfc76
ikkei12 <ikkei12.h@gmail.com>
2020-12-29 16:37:51 +0900

    add product detail page

--- components/v1/molecules/ProductsListItem.vue
+++ components/v1/molecules/ProductsListItem.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="list-item" @mouseover="onMouseOver">
-    <p class="list-item__index">{{ itemIndex }}.</p>
-    <p class="list-item__text">{{ product.title }}</p>
-  </div>
+  <nuxt-link class="list-item__link" :to="'/products/' + product.id">
+    <div class="list-item" @mouseover="onMouseOver">
+      <p class="list-item__index">{{ itemIndex }}.</p>
+      <p class="list-item__text">{{ product.title }}</p>
+    </div>
+  </nuxt-link>
 </template>
 <script lang="ts">
 import { defineComponent, PropType, computed } from '@vue/composition-api'
@@ -31,20 +33,27 @@ export default defineComponent({
 })
 </script>
 <style scoped lang="scss">
-.list-item {
-  display: flex;
-  align-items: center;
-  padding: 3vh 2vw;
-  cursor: pointer;
-  border-bottom: 2px solid rgb(220, 220, 220);
-  .list-item__index {
-    font-weight: bold;
-    font-size: 1rem;
-    margin-right: 3vw;
-  }
-  .list-item__text {
-    font-weight: bold;
-    font-size: 1.5rem;
+.list-item__link {
+  text-decoration: none;
+
+  .list-item {
+    display: flex;
+    align-items: center;
+    padding: 3vh 2vw;
+    cursor: pointer;
+    border-bottom: 2px solid rgb(220, 220, 220);
+
+    .list-item__index {
+      font-weight: bold;
+      font-size: 1rem;
+      margin-right: 3vw;
+      color: black;
+    }
+    .list-item__text {
+      font-weight: bold;
+      font-size: 1.5rem;
+      color: black;
+    }
   }
 }
 </style>
