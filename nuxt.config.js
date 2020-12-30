e4efc24e01339179f640d7a068df7c8cc794842d
ikkei12 <ikkei12.h@gmail.com>
2020-12-30 19:57:14 +0900

    install nuxt/content

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/dotenv',
+    '@nuxt/content',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
