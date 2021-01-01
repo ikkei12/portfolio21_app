6b79b0d21a4f1f307117861c048f10ca73f04c96
ikkei12 <ikkei12.h@gmail.com>
2021-01-01 21:11:18 +0900

    add article type

--- pages/articles.vue
+++ pages/articles.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="container">
-    <div v-for="(article, i) in articles" :key="`article${i}`">
-      <p>{{ article.title }}</p>
-    </div>
+    <ArticlePage :articles="articles" />
   </div>
 </template>
 
 <script lang="ts">
 import { Context } from '@nuxt/types'
 import { IContentDocument } from '@nuxt/content/types/content'
+import ArticlePage from '@/components/v1/templates/ArticlePage.vue'
 
 import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
+  components: {
+    ArticlePage,
+  },
   async asyncData({ $content }: Context) {
     const articlesContent = await $content('articles').fetch()
     const articles = articlesContent.map((article: IContentDocument) => {
---
+++ types/ArticleCard.ts
@@ -0,0 +1,3 @@
+type ArticleCard = {
+  title: string
+}
