f19094861ccf5919a4e5671a5a8f5ba73b5d0aff
ikkei12 <ikkei12.h@gmail.com>
2021-01-04 23:37:04 +0900

    fix id page async

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,34 +1,27 @@
 <template>
   <ProductsProvider>
-    <ProductDetailPage :product="state.product" />
+    <ProductDetailPage :product="product" />
   </ProductsProvider>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from '@vue/composition-api'
+import { defineComponent } from '@vue/composition-api'
 import ProductDetailPage from '@/components/v1/templates/ProductDetailPage.vue'
 export default defineComponent({
   components: {
     ProductDetailPage,
   },
-  setup(_props, context) {
-    const path = context.root.$route.path
-    const state = reactive({
-      product: {},
-    })
-    const fetchContent = async () => {
-      await context.root.$axios
-        .get(path)
-        .then(async (res) => {
-          console.log(res.data)
-          state.product = await res.data
-        })
-        .catch((e) => {
-          console.error(e)
-        })
-    }
-    fetchContent()
+  async asyncData({ $axios, params }) {
+    const path = `/products/${params.id}`
+    const product = await $axios
+      .get(path)
+      .then((res) => {
+        return res.data
+      })
+      .catch((e) => {
+        console.error(e)
+      })
 
-    return { state }
+    return { product }
   },
 })
 </script>
