c4f7154089887814da80389bf024f0fe966d0e3a
ikkei12 <ikkei12.h@gmail.com>
2021-01-07 23:17:31 +0900

    add career tree

--- components/v1/molecules/ProfileCareerCard.vue
+++ components/v1/molecules/ProfileCareerCard.vue
@@ -1,14 +1,18 @@
 <template>
   <Card class="career__card">
     <h3 class="title">Career</h3>
+    <CareerTree />
   </Card>
 </template>
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import Card from '@/components/v1/molecules/Card.vue'
+import CareerTree from '@/components/v1/organisms/CareerTree.vue'
+
 export default defineComponent({
   components: {
     Card,
+    CareerTree,
   },
 })
 </script>
---
+++ components/v1/organisms/CareerTree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <CareerTreeNode v-for="(node, i) in nodes" :key="`node${i}`" :node="node" />
+  </div>
+</template>
+<script lang="ts">
+import CareerTreeNode from '@/components/v1/molecules/CareerTreeNode.vue'
+import { defineComponent, PropType } from '@vue/composition-api'
+export default defineComponent({
+  props: {
+    nodes: {
+      type: Array as PropType<Node[]>,
+    },
+  },
+  components: {
+    CareerTreeNode,
+  },
+})
+</script>
