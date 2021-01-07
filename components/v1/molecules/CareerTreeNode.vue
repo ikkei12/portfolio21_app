d5f207775e45e24a07b4fb5ebda3a1c02bc76023
ikkei12 <ikkei12.h@gmail.com>
2021-01-08 00:32:13 +0900

    add dots

--- components/v1/molecules/CareerTreeNode.vue
+++ components/v1/molecules/CareerTreeNode.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <Dot :size="18" color="grey" />
-    <p>{{ node.date }}</p>
-    <p>{{ node.title }}</p>
-    <p>{{ node.description }}</p>
+  <div class="node">
+    <div class="node__inner">
+      <p class="date">{{ node.date }}</p>
+      <p class="title">{{ node.title }}</p>
+    </div>
+    <div class="description__wrapper">
+      <p>{{ node.description }}</p>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api'
-import Dot from '@/components/v1/atoms/Dot.vue'
 export default defineComponent({
-  components: {
-    Dot,
-  },
   props: {
     node: {
       type: Object as PropType<CareerNode>,
@@ -20,3 +19,20 @@ export default defineComponent({
   },
 })
 </script>
+<style lang="scss" scoped>
+.node {
+  .node__inner {
+    display: flex;
+    .date {
+      margin-right: 8px;
+      font-size: 14px;
+    }
+    .title {
+      font-size: 15px;
+    }
+  }
+  .description__wrapper {
+    height: 18px;
+  }
+}
+</style>
--- components/v1/organisms/CareerTree.vue
+++ components/v1/organisms/CareerTree.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    <CareerTreeNode
-      v-for="(node, i) in careerNodes"
-      :key="`node${i}`"
-      :node="node"
-    />
+  <div class="career-tree">
+    <div class="dots-group">
+      <Dot
+        v-for="(dot, i) in careerNodes"
+        :key="`dot${i}`"
+        :size="18"
+        class="career-tree__dot"
+        :class="{ '--w-desc': dot.description }"
+        color="grey"
+      />
+    </div>
+    <div>
+      <CareerTreeNode
+        v-for="(node, i) in careerNodes"
+        :key="`node${i}`"
+        :node="node"
+        class="career-tree__node"
+        :class="{ '--w-desc': node.description }"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import CareerTreeNode from '@/components/v1/molecules/CareerTreeNode.vue'
 import { defineComponent, PropType } from '@vue/composition-api'
+import Dot from '@/components/v1/atoms/Dot.vue'
+
 export default defineComponent({
   props: {
     careerNodes: {
@@ -18,6 +34,25 @@ export default defineComponent({
   },
   components: {
     CareerTreeNode,
+    Dot,
   },
 })
 </script>
+<style lang="scss" scoped>
+.career-tree {
+  display: flex;
+  .career-tree__dot {
+    margin-bottom: 8px;
+    &.--w-desc {
+      margin-bottom: 26px;
+    }
+  }
+  .career-tree__node {
+    margin-bottom: 8px;
+    height: 18px;
+    &.--w-desc {
+      height: 36px;
+    }
+  }
+}
+</style>
