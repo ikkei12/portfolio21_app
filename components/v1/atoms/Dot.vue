3d8626d67e1465141d3481e0daaaa7636650b004
ikkei12 <ikkei12.h@gmail.com>
2021-01-08 00:01:16 +0900

    add dot

---
+++ components/v1/atoms/Dot.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dot" :style="`width:${size}px; height:${size}px;`">
+    <div
+      :style="`width:${size / 1.8}px; height:${
+        size / 1.8
+      }px; background:${color}`"
+      class="dot__core"
+    ></div>
+    <div class="dot__outline" :style="`border:2px solid ${color};`"></div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    size: { type: Number },
+    color: { type: String },
+  },
+})
+</script>
+<style scoped lang="scss">
+.dot {
+  border-radius: 50%;
+  position: relative;
+  .dot__core {
+    border-radius: 50%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .dot__outline {
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    border-radius: 50%;
+  }
+}
+</style>
--- components/v1/molecules/CareerTreeNode.vue
+++ components/v1/molecules/CareerTreeNode.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Dot :size="18" color="grey" />
     <p>{{ node.date }}</p>
     <p>{{ node.title }}</p>
     <p>{{ node.description }}</p>
@@ -7,8 +8,11 @@
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api'
-
+import Dot from '@/components/v1/atoms/Dot.vue'
 export default defineComponent({
+  components: {
+    Dot,
+  },
   props: {
     node: {
       type: Object as PropType<CareerNode>,
