795447af570b95f05247fa73edb8e33a6da2fca1
ikkei12 <ikkei12.h@gmail.com>
2021-01-16 17:47:59 +0900

    fix ogp card

--- components/global/OgpCard.vue
+++ components/global/OgpCard.vue
@@ -1,28 +1,29 @@
 <template>
-  <a
-    :href="url"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="ogp-card__link"
-  >
-    <div class="ogp-card">
-      <img :src="ogp.image" class="image" />
-      <div class="ogp-card__inner">
-        <h3 class="ogp-card__title">{{ ogp.title }}</h3>
-        <p class="ogp-card__description">{{ ogp.description }}</p>
+  <div class="ogp-card__wrapper">
+    <a
+      :href="url"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="ogp-card__link"
+    >
+      <div class="ogp-card">
+        <img :src="ogp.image" class="image" />
+        <div class="ogp-card__inner">
+          <h3 class="ogp-card__title">{{ ogp.title }}</h3>
+          <p class="ogp-card__description">{{ ogp.description }}</p>
+        </div>
       </div>
-    </div>
-  </a>
+    </a>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from '@vue/composition-api'
+import { defineComponent, onMounted, reactive } from '@vue/composition-api'
 
 export default defineComponent({
   props: {
     url: { type: String },
   },
   setup(props, context) {
-    const url = props.url
     const ogp = reactive<{
       title: string | null
       description: string | null
@@ -32,80 +33,103 @@ export default defineComponent({
       description: '',
       image: '',
     })
-    if (!url) return
-    context.root.$axios
-      .post(
-        'https://asia-northeast1-portfolio21-56e7e.cloudfunctions.net/getOgpInfo',
-        { url },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-      .then((res) => {
-        ogp.image = res.data.ogp.image
-        ogp.description = res.data.ogp.desc
-        ogp.title = res.data.ogp.title
-      })
-      .catch((e) => console.error(e))
+
+    onMounted(() => {
+      const url = props.url
+      if (!url) return
+      context.root.$axios
+        .post(
+          'https://asia-northeast1-portfolio21-56e7e.cloudfunctions.net/getOgpInfo',
+          { url },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then((res) => {
+          ogp.image = res.data.ogp.image
+          ogp.description = res.data.ogp.description
+          ogp.title = res.data.ogp.title
+        })
+        .catch((e) => console.error(e))
+    })
     return { ogp }
   },
 })
 </script>
 <style scoped lang="scss">
-.ogp-card__link {
-  text-decoration: none;
-  color: #333;
-  display: block;
-  margin: 60px 0;
-  &:hover {
-    box-shadow: 0 0 20px rgba(173, 173, 173, 0.285);
-  }
-  .ogp-card {
-    display: flex;
-    height: 150px;
-    border: 1px solid #efefef;
-    border-radius: 3px;
-
-    .image {
-      height: 150px;
-      width: 150px;
-      object-fit: cover;
-      margin-bottom: 0;
+.ogp-card__wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  .ogp-card__link {
+    text-decoration: none;
+    color: #333;
+    display: block;
+    margin: 60px 0;
+    &:hover {
+      box-shadow: 0 0 20px rgba(173, 173, 173, 0.285);
     }
-    .ogp-card__inner {
-      height: 100%;
-      padding: 8px 12px;
-      overflow: hidden;
-      .ogp-card__title {
-        line-height: initial;
-        margin-bottom: 10px;
-      }
-      .ogp-card__description {
-        font-size: 13px;
-        line-height: 1.1;
-        color: grey;
+    .ogp-card {
+      display: flex;
+      height: 150px;
+      border: 1px solid #efefef;
+      border-radius: 3px;
+
+      .image {
+        height: 150px;
+        width: 150px;
+        object-fit: cover;
         margin-bottom: 0;
+      }
+      .ogp-card__inner {
+        height: 100%;
+        padding: 8px 12px;
+        overflow: hidden;
+        .ogp-card__title {
+          line-height: initial;
+          margin-bottom: 10px;
+        }
+        .ogp-card__description {
+          font-size: 13px;
+          line-height: 1.1;
+          color: grey;
+          margin-bottom: 0;
+        }
       }
     }
   }
 }
 @include tab {
-  .ogp-card__link {
-    .ogp-card {
-      display: table;
-      .image {
+  .ogp-card__wrapper {
+    .ogp-card__link {
+      width: 60%;
+      .ogp-card {
+        display: table;
         width: 100%;
-      }
-      .ogp-card__inner {
-        .ogp-card__title {
-          font-size: 13px;
+        .image {
+          height: 200px;
+          width: 100%;
+          border-top-right-radius: 5px;
+          border-top-left-radius: 5px;
         }
-        .ogp-card__description {
-          font-size: 11px;
+        .ogp-card__inner {
+          .ogp-card__title {
+            font-size: 13px;
+          }
+          .ogp-card__description {
+            font-size: 11px;
+          }
         }
       }
+    }
+  }
+}
+@include sp {
+  .ogp-card__wrapper {
+    .ogp-card__link {
+      width: 95%;
     }
   }
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,106 +1,125 @@
-import * as fs from 'fs'
 import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
-import { createCanvas, loadImage } from 'canvas'
+// import * as fs from 'fs'
+// import * as admin from 'firebase-admin'
+// import { createCanvas, loadImage } from 'canvas'
+const cors = require('cors')({ origin: true })
 
-// const cors = require('cors')({ origin: true })
+const notifySlack = (message: String) => {
+  const { IncomingWebhook } = require('@slack/webhook')
+  const webhook = new IncomingWebhook(functions.config().slack.webhookurl)
+  ;(async () => {
+    await webhook.send({
+      text: message,
+    })
+  })()
+}
+
 export const getOgpInfo = functions
   .region('asia-northeast1')
-  .https.onRequest(async (req, res) => {
-    // return cors(req, res, async () => {
-    res.set('Access-Control-Allow-Origin', '*')
+  .https.onRequest((req, res) => {
+    return cors(req, res, async () => {
+      res.set('Access-Control-Allow-Origin', '*')
+      res.set('Access-Control-Allow-Methods', 'POST')
+      res.set('Access-Control-Allow-Headers', 'Content-Type')
+      res.set('Access-Control-Max-Age', '3600')
 
-    // if (req.method === 'OPTIONS') {
-    // Send res to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET')
-    res.set('Access-Control-Allow-Headers', 'Content-Type')
-    res.set('Access-Control-Max-Age', '3600')
-    // res.status(204).send('')
-    // }
+      const cheerio = require('cheerio')
+      const request = require('request')
+      const url = req.body.url
+      console.log(url)
 
-    // res.header('Access-Control-Allow-Origin', '*')
-    // res.header(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept'
-    // )
-    const cheerio = require('cheerio')
-    const request = require('request')
-    const url = req.body.url
-    await request(url, (e: any, _res: any, html: any) => {
-      if (e) {
-        console.error(e)
-      }
-      try {
-        const ogp = {
-          title: '',
-          description: '',
-          image: '',
+      await request(url, (e: any, _res: any, html: any) => {
+        if (e) {
+          console.error(e)
+          notifySlack(e)
         }
-        const $ = cheerio.load(html)
-        ogp.title = $('title').text()
-        ogp.image = $("meta[property='og:image']").attr('content')
-        ogp.description = $("meta[property='og:description']").attr('content')
+        try {
+          const ogp = {
+            title: '',
+            description: '',
+            image: '',
+          }
+          const $ = cheerio.load(html)
+          ogp.title = $('title').text()
+          ogp.image = $("meta[property='og:image']").attr('content')
+          ogp.description = $("meta[property='og:description']").attr('content')
 
-        res.send({ ogp })
-      } catch (e) {
-        console.error(e)
-        res.status(500).send({ error: e })
-      }
+          res.send({ ogp })
+        } catch (e) {
+          notifySlack(e)
+          res.status(500).send({ error: e })
+        }
+      })
     })
   })
-// })
 
+// /**
+//  * API
+//  * @param title OGPを作る文字列
+//  * @param questionId questionID
+//  */
+// export const createOgpImageAndSave = functions.https.onRequest(
+//   (request, response) => {
+//     const createOGP = async (title) => {
+//       const W = 1200
+//       const H = 630
 
+//       const canvas = createCanvas(W, H)
+//       const ctx = canvas.getContext('2d')
 
-/**
- * API
- * @param title OGPを作る文字列
- * @param questionId questionID
- */
-export const createOgpImageAndSave = functions.https.onRequest(
-  (request, response) => {
-    const createOGP = async (title) => {
-      const W = 1200
-      const H = 630
-    
-      const canvas = createCanvas(W, H)
-      const ctx = canvas.getContext('2d')
-    
-      // 背景画像の描画
-      const baseImage = await loadImage(baseImagePath)
-      ctx.drawImage(baseImage, 0, 0, W, H)
-    
-      return canvas.toBuffer()
-    }
-    
-    const upload = async (image: Buffer): Promise<void> => {
-      const loaclTargetPath = `/tmp/target.png`
-      const localBasePath = '/tmp/base.png'
-      const targetPath = `${CLOUD_STORAGE_KEYS.QUESTION}/ogp.png`
-      const basePath = `${CLOUD_STORAGE_KEYS.QUESTION}/base.png`
-    
-      // （Syncはやめといた方が良いよ）
-      fs.writeFileSync(loaclTargetPath, image)
-    
-      // Storageにアップロード
-      await bucket.upload(loaclTargetPath, { destination: targetPath })
-    
-      // tmpファイルの削除
-      fs.unlinkSync(localBasePath)
-      fs.unlinkSync(loaclTargetPath)
-    response.set('Access-Control-Allow-Origin', '*')
+//       // 背景画像の描画
+//       const baseImage = await loadImage(baseImagePath)
+//       ctx.drawImage(baseImage, 0, 0, W, H)
 
-    if (request.method === 'OPTIONS') {
-      // Send response to OPTIONS requests
-      response.set('Access-Control-Allow-Methods', 'GET')
-      response.set('Access-Control-Allow-Headers', 'Content-Type')
-      response.set('Access-Control-Max-Age', '3600')
-      response.status(204).send('')
-    } else {
-      const body = request.body
-      const image = await createOgp(body.title)
-      await upload(image)
-      response.send('ok')
-    }
-  }
-)
+//       return canvas.toBuffer()
+//     }
+
+//     const upload = async (image: Buffer): Promise<void> => {
+//       const loaclTargetPath = `/tmp/target.png`
+//       const localBasePath = '/tmp/base.png'
+//       const targetPath = `${CLOUD_STORAGE_KEYS.QUESTION}/ogp.png`
+//       const basePath = `${CLOUD_STORAGE_KEYS.QUESTION}/base.png`
+
+//       // （Syncはやめといた方が良いよ）
+//       fs.writeFileSync(loaclTargetPath, image)
+
+//       // Storageにアップロード
+//       await bucket.upload(loaclTargetPath, { destination: targetPath })
+
+//       // tmpファイルの削除
+//       fs.unlinkSync(localBasePath)
+//       fs.unlinkSync(loaclTargetPath)
+//     response.set('Access-Control-Allow-Origin', '*')
+
+//     if (request.method === 'OPTIONS') {
+//       // Send response to OPTIONS requests
+//       response.set('Access-Control-Allow-Methods', 'GET')
+//       response.set('Access-Control-Allow-Headers', 'Content-Type')
+//       response.set('Access-Control-Max-Age', '3600')
+//       response.status(204).send('')
+//     } else {
+//       const body = request.body
+//       const image = await createOgp(body.title)
+//       await upload(image)
+//       response.send('ok')
+//     }
+//   }
+// )
+
+// FIX: ベータ版の機能だからかfirebase-functionsの現バージョンで使えない？
+// exports.sendOnRegressedIssue = functions.crashlytics
+//   .issue()
+//   .onRegressed(async (issue) => {
+//     const issueId = issue.issueId
+//     const issueTitle = issue.issueTitle
+//     const appName = issue.appInfo.appName
+//     const appPlatform = issue.appInfo.appPlatform
+//     const latestAppVersion = issue.appInfo.latestAppVersion
+
+//     const slackMessage =
+//       `<!here|here> There is a new issue - ${issueTitle} (${issueId}) ` +
+//       `in ${appName}, version ${latestAppVersion} on ${appPlatform}`
+
+//     await notifySlack(slackMessage)
+//     console.log(`Posted new issue ${issueId} successfully to Slack`)
+//   })
