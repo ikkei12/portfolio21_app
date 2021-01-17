7eb6c77ac2d37f1c9b69758d9199bf6a6ebb9f6e
ikkei12 <ikkei12.h@gmail.com>
2021-01-17 22:03:13 +0900

    fix ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
       {
         property: 'og:image',
         content:
-          'https://storage.cloud.google.com/portfolio21-56e7e.appspot.com/ogp/files/article-1.png?authuser=1',
+          'https://storage.googleapis.com/portfolio21-56e7e.appspot.com/ogp/files/article-1.png?authuser=1',
       },
       { name: 'twitter:title', content: "1keiuu's portfolio" },
       {
