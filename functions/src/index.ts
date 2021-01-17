46c117bbbf9e3b94efb64ade22e79cfc875e79ea
ikkei12 <ikkei12.h@gmail.com>
2021-01-17 15:24:09 +0900

    add create ogp functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
+import * as fs from 'fs'
 import * as functions from 'firebase-functions'
-// import * as fs from 'fs'
-// import * as admin from 'firebase-admin'
-// import { createCanvas, loadImage } from 'canvas'
+import * as admin from 'firebase-admin'
+import { createCanvas, loadImage } from 'canvas'
 const cors = require('cors')({ origin: true })
 
 const notifySlack = (message: String) => {
@@ -53,58 +53,70 @@ export const getOgpInfo = functions
     })
   })
 
-// /**
-//  * API
-//  * @param title OGPを作る文字列
-//  * @param questionId questionID
-//  */
-// export const createOgpImageAndSave = functions.https.onRequest(
-//   (request, response) => {
-//     const createOGP = async (title) => {
-//       const W = 1200
-//       const H = 630
+/**
+ * API
+ * @param title OGPを作る文字列
+ * @param questionId questionID
+ */
+export const createOgpImageAndSave = functions.https.onRequest(
+  (request, response) => {
+    return cors(request, response, async () => {
+      const createOgp = async (title: string, name: string) => {
+        const baseImagePath = `${
+          functions.config().cloud_storsge.path
+        }/ogp/base.png`
+        const W = 1200
+        const H = 630
+        console.log(title)
+        console.log(name)
+        const canvas = createCanvas(W, H)
+        const ctx = canvas.getContext('2d')
 
-//       const canvas = createCanvas(W, H)
-//       const ctx = canvas.getContext('2d')
+        // 背景画像の描画
+        const baseImage = await loadImage(baseImagePath)
+        ctx.drawImage(baseImage, 0, 0, W, H)
 
-//       // 背景画像の描画
-//       const baseImage = await loadImage(baseImagePath)
-//       ctx.drawImage(baseImage, 0, 0, W, H)
+        return canvas.toBuffer()
+      }
 
-//       return canvas.toBuffer()
-//     }
+      const upload = async (image: Buffer, slug: string): Promise<void> => {
+        const loaclTargetPath = `/tmp/target.png`
+        const localBasePath = '/tmp/base.png'
+        const targetPath = `${
+          functions.config().cloud_storsge.path
+        }/ogp/files/${slug}.png`
 
-//     const upload = async (image: Buffer): Promise<void> => {
-//       const loaclTargetPath = `/tmp/target.png`
-//       const localBasePath = '/tmp/base.png'
-//       const targetPath = `${CLOUD_STORAGE_KEYS.QUESTION}/ogp.png`
-//       const basePath = `${CLOUD_STORAGE_KEYS.QUESTION}/base.png`
+        // （Syncはやめといた方が良いよ）
+        fs.writeFileSync(loaclTargetPath, image)
 
-//       // （Syncはやめといた方が良いよ）
-//       fs.writeFileSync(loaclTargetPath, image)
+        // Storageにアップロード
+        await admin
+          .storage()
+          .bucket(functions.config().cloud_storsge.bucket)
+          .upload(loaclTargetPath, { destination: targetPath })
 
-//       // Storageにアップロード
-//       await bucket.upload(loaclTargetPath, { destination: targetPath })
+        // tmpファイルの削除
+        fs.unlinkSync(localBasePath)
+        fs.unlinkSync(loaclTargetPath)
+      }
 
-//       // tmpファイルの削除
-//       fs.unlinkSync(localBasePath)
-//       fs.unlinkSync(loaclTargetPath)
-//     response.set('Access-Control-Allow-Origin', '*')
+      response.set('Access-Control-Allow-Origin', '*')
 
-//     if (request.method === 'OPTIONS') {
-//       // Send response to OPTIONS requests
-//       response.set('Access-Control-Allow-Methods', 'GET')
-//       response.set('Access-Control-Allow-Headers', 'Content-Type')
-//       response.set('Access-Control-Max-Age', '3600')
-//       response.status(204).send('')
-//     } else {
-//       const body = request.body
-//       const image = await createOgp(body.title)
-//       await upload(image)
-//       response.send('ok')
-//     }
-//   }
-// )
+      if (request.method === 'OPTIONS') {
+        // Send response to OPTIONS requests
+        response.set('Access-Control-Allow-Methods', 'GET')
+        response.set('Access-Control-Allow-Headers', 'Content-Type')
+        response.set('Access-Control-Max-Age', '3600')
+        response.status(204).send('')
+      } else {
+        const body = request.body
+        const image = await createOgp(body.title, body.name)
+        await upload(image, body.slug)
+        response.send('ok')
+      }
+    })
+  }
+)
 
 // FIX: ベータ版の機能だからかfirebase-functionsの現バージョンで使えない？
 // exports.sendOnRegressedIssue = functions.crashlytics
