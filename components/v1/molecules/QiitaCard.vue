3b4a5b97e50ed4b49e90e9e5cba5329dfa4ccb8a
ikkei12 <ikkei12.h@gmail.com>
2021-01-22 03:22:49 +0900

    add qiita card

--- components/v1/molecules/QiitaCard.vue
+++ components/v1/molecules/QiitaCard.vue
@@ -1,12 +1,20 @@
 <template>
-  <div v-if="qiita.isShow">
-    <p>{{ qiita.title }}</p>
+  <div v-if="qiita.isShow" class="qiita-card">
+    <img :src="qiita.image" class="thumbnail" width="300" />
+    <div class="inner">
+      <p>{{ qiita.title }}</p>
+      <div class="likes-count__wrapper">
+        <Icon icon-name="heart" color="#FF846B" />
+        <p class="likes-count__text">{{ qiita.likesCount }}</p>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api'
-
+import Icon from '@/components/v1/atoms/Icon.vue'
 export default defineComponent({
+  components: { Icon },
   props: {
     qiita: {
       type: Object as PropType<QiitaArticle>,
@@ -15,3 +23,32 @@ export default defineComponent({
   },
 })
 </script>
+<style scoped lang="scss">
+.qiita-card {
+  background: white;
+  width: 300px;
+  border-radius: 8px;
+  margin-left: 16px;
+  display: inline-table;
+  .thumbnail {
+    width: 100%;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+  .inner {
+    height: 100px;
+    padding: 8px;
+    p {
+      font-size: 14px;
+    }
+    .likes-count__wrapper {
+      display: flex;
+      align-items: center;
+      margin-top: 4px;
+      .likes-count__text {
+        margin-left: 5px;
+      }
+    }
+  }
+}
+</style>
