4955ec28086db6011eaeb25308124da79bd606ae
ikkei12 <ikkei12.h@gmail.com>
2021-02-05 18:54:33 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page__inner">
+    <h2 class="title">お探しのページが見つかりませんでした</h2>
+    <p>URLに間違いないか確認し、再度アクセスを試してください。</p>
+    <p>また、つながらない場合は時間をおいて再度アクセスしてください。</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page__inner {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .title {
+    margin-bottom: 50px;
+    color: #333;
+  }
+}
+</style>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  layout: 'error',
+  props: ['error'],
+})
+</script>
