ae4c25047109a035a9d3c7ad90a129e2e2e2825f
1keiuu <ikkei12.h@gmail.com>
2021-05-21 12:14:54 +0900

    display thumbnail

--- components/v1/molecules/ProfileThumbnail.vue
+++ components/v1/molecules/ProfileThumbnail.vue
@@ -1,103 +1,63 @@
 <template>
-  <div class="image-wrapper">
-    <img
-      src="~/assets/images/top/1.webp"
-      class="icon"
-      alt="icon"
-      height="490px"
-      width="700px"
-    />
+  <div class="thumbnail">
+    <div class="image-wrapper" :class="{ '--active': activeIndex == 0 }">
+      <img
+        src="~/assets/images/top/1.webp"
+        class="icon"
+        alt="icon"
+        height="490px"
+        width="700px"
+      />
+    </div>
+    <div class="image-wrapper" :class="{ '--active': activeIndex == 1 }">
+      <img
+        src="~/assets/images/top/2.webp"
+        class="icon"
+        alt="icon"
+        height="600px"
+        width="400px"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, onUnmounted, ref } from '@vue/composition-api'
 
 export default defineComponent({
   components: {},
+  setup() {
+    const activeIndex = ref(0)
+    const timerID = setInterval(() => {
+      if (activeIndex.value >= 1) activeIndex.value = 0
+      else activeIndex.value++
+    }, 5000)
+    onUnmounted(() => {
+      window.clearInterval(timerID)
+    })
+    return { activeIndex }
+  },
 })
 </script>
 <style scoped lang="scss">
-.image-wrapper {
-  position: absolute;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: -1;
-  margin: auto;
-  height: fit-content;
-}
-.info__card {
-  width: 75%;
-  display: flex;
-  justify-content: space-between;
-
-  .inner {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-
-    .name-en {
-      font-size: 2.5rem;
-      white-space: nowrap;
-      font-family: Roboto;
-      font-style: italic;
-      font-weight: 500;
-    }
-
-    .title {
-      font-size: 48px;
-      font-family: Roboto;
-      font-style: italic;
-      font-weight: 500;
-      margin: 30px 0;
-      line-height: 50px;
-    }
-
-    .link {
-      text-decoration: none;
-      color: black;
-      margin-top: 40px;
-
-      .link__inner {
-        background: #c62828;
-        display: flex;
-        align-items: center;
-        border-radius: 4px;
-        padding: 4px 16px 4px 4px;
-
-        .icon__wrapper {
-          height: 30px;
-          width: 30px;
-          display: block;
-
-          .icon {
-            height: 100%;
-            width: 100%;
-            border-radius: 50%;
-          }
-        }
-
-        .contact-msg {
-          color: #fff;
-          font-weight: bold;
-        }
-      }
-
-      p {
-        margin-left: 8px;
-      }
+.thumbnail {
+  height: 100%;
+  .image-wrapper {
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: -1;
+    margin: auto;
+    height: fit-content;
+    opacity: 0;
+    transition: 1.5s;
+    &.--active {
+      opacity: 1;
+      transition: 1.5s;
     }
   }
 }
 
 @include sp {
-  .info__card {
-    .inner {
-      .link {
-        margin-top: 2vh;
-      }
-    }
-  }
 }
 </style>
